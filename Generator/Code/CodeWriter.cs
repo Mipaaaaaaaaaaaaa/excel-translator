74a0b6b721d7af4fb9e51e28f62a6ee39384c0d2
魔法咪啪咪啪 <39038341+Mipaaaaaaaaaaaaa@users.noreply.github.com>
2021-08-21 15:39:39 +0800

    修复名称错误

--- Generator/Code/CodeWriter.cs
+++ Generator/Code/CodeWriter.cs
@@ -32,7 +32,7 @@ namespace ExcelTranslator.Generator.Code {
             } else if(ExcelUtil.IsParamSheet(dataTable.TableName))
             {
                 /* 生成常量类 */
-                string className = options.ParamNamePrefix + dataTable.TableName;
+                string className = options.ParamNamePrefix + dataTable.TableName.Substring(5);
                 List<ParamMember> fields = CodeUtil.GetParamMembers(dataTable);
                 builder.AppendFormat("    /// <summary> Generate From {0} </summary>", excelName).AppendLine();
                 builder.AppendFormat("    public static class {0} {{", className).AppendLine();
